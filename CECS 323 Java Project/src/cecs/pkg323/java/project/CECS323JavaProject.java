dd96059056decc63b5e45df0de2db1f70a2bd6e3
Julian Poyourow <julianpoyo@gmail.com>
2017-03-18 15:22:00 -0700

    Added listAllPublishers

--- CECS 323 Java Project/src/cecs/pkg323/java/project/CECS323JavaProject.java
+++ CECS 323 Java Project/src/cecs/pkg323/java/project/CECS323JavaProject.java
@@ -40,6 +40,7 @@ public class CECS323JavaProject {
             System.out.println("Main Menu");
             System.out.println("1. List all writing groups");
             System.out.println("2. List info for specific group");
+            System.out.println("3. List all publishers");
 
             String menuChoice = in.nextLine();
 
@@ -93,6 +94,29 @@ public class CECS323JavaProject {
                                 dispNull(groupName), dispNull(headWriter), dispNull(yearFormed), dispNull(subject));
                     }
                     rs.close();
+                    mainMenu(conn);
+                    break;
+                case "3":
+                    pstmt = conn.prepareStatement(
+                        "SELECT publisherName, publisherAddress, publisherPhone, publisherEmail FROM Publishers"
+                    );
+                    rs = pstmt.executeQuery();
+
+                    displayFormat = "%-25s%-25s%-20s%-20s\n";
+
+                    System.out.printf(displayFormat, "publisherName", "publisherAddress", "publisherPhone", "publisherEmail");
+                    while (rs.next()) {
+                        //Retrieve by column name
+                        String publisherName = rs.getString("publisherName");
+                        String publisherAddress = rs.getString("publisherAddress");
+                        String publisherPhone = rs.getString("publisherPhone");
+                        String publisherEmail = rs.getString("publisherEmail");
+
+                        //Display values
+                        System.out.printf(displayFormat, 
+                                dispNull(publisherName), dispNull(publisherAddress), dispNull(publisherPhone), dispNull(publisherEmail));
+                    }
+
                     mainMenu(conn);
                     break;
                 default:
