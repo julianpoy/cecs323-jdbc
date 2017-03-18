b7cfcdeaa65933419d6ab9114a9a5ab07177b0ca
Julian Poyourow <julianpoyo@gmail.com>
2017-03-18 14:22:59 -0700

    Added initialization SQL

---
+++ CECS 323 Java Project/src/cecs/pkg323/java/project/init.sql
@@ -0,0 +1,26 @@
+CREATE TABLE WritingGroup(
+  groupName varchar(200) NOT NULL,
+  headWriter varchar(200) NOT NULL,
+  yearFormed date NOT NULL,
+  subject varchar(200) NOT NULL,
+  PRIMARY KEY(groupName)
+);
+
+CREATE TABLE Publishers(
+  publisherName varchar(200) NOT NULL,
+  publisherAddress varchar(500) NOT NULL,
+  publisherPhone varchar(200) NOT NULL,
+  publisherEmail varchar(200) NOT NULL,
+  PRIMARY KEY(publisherName)
+);
+
+CREATE TABLE Books(
+  groupName varchar(200) NOT NULL,
+  bookTitle varchar(200) NOT NULL,
+  publisherName varchar(200) NOT NULL,
+  yearPublished varchar(200) NOT NULL,
+  numberPages integer NOT NULL,
+  PRIMARY KEY(groupName, bookTitle)
+  FOREIGN KEY(groupName) REFERENCES WritingGroup(groupName),
+  FOREIGN KEY(publisherName) REFERENCES Publishers(publisherName)
+);
